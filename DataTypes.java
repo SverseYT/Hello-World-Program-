a983318002ea84f828ccb8f267307e8104f888b7
SverseYT <98480197+SverseYT@users.noreply.github.com>
2023-10-30 09:28:09 -0700

    Add files via upload

---
+++ DataTypes.java
@@ -0,0 +1,15 @@
+class DataTypes {
+    public static void main(String[] args) {
+        final int[] INPUT = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+int sum = 0;
+String sumString = "0";
+double sumDouble = 0.0;
+
+// Loop goes here 
+
+System.out.println("Sum as int: " + sum);
+        System.out.println("Sum as String: " + sumString); 
+        System.out.println("Sum as double: " + sumDouble);
+    }
+}
